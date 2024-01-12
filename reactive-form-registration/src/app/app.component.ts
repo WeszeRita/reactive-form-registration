ade6a551e50924bece67e0e98e2536e7b448c8af
WeszeRita <weszelovszkirita@gmail.com>
2024-01-12 18:02:08 +0100

    reg form

--- reactive-form-registration/src/app/app.component.ts
+++ reactive-form-registration/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'reactive-form-registration';
+export class AppComponent implements OnInit {
+  registrationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      username: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]],
+      confirmPassword: [null, Validators.required],
+      address: this.formBuilder.group({
+        street: [null],
+        city: [null],
+        state: [null],
+        zip: [null, [Validators.min(1011), Validators.max(9985)]],
+        phoneNumber: [null, Validators.pattern('^[1-9][0-9]{8}$')],
+      }),
+    })
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password').value;
+    const confirmPassword = control.get('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+      confirmPassword.setErrors({ passwordMismatchError: true })
+    }
+  };
+
+  onSubmit(): void {
+    console.log('Form values:', this.registrationForm.value);
+    this.registrationForm.reset();
+  }
 }
--- reactive-form-registration/src/app/app.module.ts
+++ reactive-form-registration/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
