91aebc7b717b1b403e1d6e9487a047ef8f626ded
WeszeRita <weszelovszkirita@gmail.com>
2024-01-15 11:06:03 +0100

    reg form

--- reactive-form-registration/src/app/app.component.ts
+++ reactive-form-registration/src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,31 +8,63 @@ import {
 })
 export class AppComponent implements OnInit {
   registrationForm: FormGroup;
+  private readonly passwordRegEx = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$';
+  private readonly phoneRegEx = '^[1-9][0-9]{8}$';
 
-  constructor(private formBuilder: FormBuilder) {}
+  get usernameControl(): AbstractControl<string> {
+    return this.registrationForm.get('username');
+  }
+
+  get emailControl(): AbstractControl<string> {
+    return this.registrationForm.get('email');
+  }
+
+  get passwordControl(): AbstractControl<string> {
+    return this.registrationForm.get('credentials.password');
+  }
+
+  get confirmPasswordControl(): AbstractControl<string> {
+    return this.registrationForm.get('credentials.confirmPassword');
+  }
+
+  get zipControl(): AbstractControl<string> {
+    return this.registrationForm.get('address.zip');
+  }
+
+  get phoneNumberControl(): AbstractControl<string> {
+    return this.registrationForm.get('address.phoneNumber');
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.registrationForm = this.formBuilder.group({
       username: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]],
-      confirmPassword: [null, Validators.required],
+      credentials: this.formBuilder.group({
+          password: [null, [Validators.required, Validators.pattern(this.passwordRegEx)]],
+          confirmPassword: [null],
+        },
+        {
+          validators: this.passwordMatchValidator
+        }),
       address: this.formBuilder.group({
         street: [null],
         city: [null],
         state: [null],
         zip: [null, [Validators.min(1011), Validators.max(9985)]],
-        phoneNumber: [null, Validators.pattern('^[1-9][0-9]{8}$')],
+        phoneNumber: [null, Validators.pattern(this.phoneRegEx)],
       }),
-    })
+    });
   }
 
   passwordMatchValidator(control: AbstractControl) {
-    const password = control.get('password').value;
-    const confirmPassword = control.get('confirmPassword').value;
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
 
-    if (password !== confirmPassword) {
-      confirmPassword.setErrors({ passwordMismatchError: true })
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatchError: true });
     }
   };
 
